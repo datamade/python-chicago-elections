b253de0811893a10fbe3ed672cfe53c8c545916a
hannah cushman garland <hannah.cushman@gmail.com>
2023-03-31 13:58:28 -0500

    Update precinct result scraper

--- chi_elections/precincts.py
+++ chi_elections/precincts.py
@@ -8,7 +8,7 @@ import lxml.html
 import requests
 
 class Election(object):
-    ELECTION_URL = 'https://chicagoelections.com/en/election-results.asp'
+    ELECTION_URL = 'https://chicagoelections.gov/en/election-results.asp'
     
     def __init__(self, elec_code, name, session):
         self.elec_code = elec_code
@@ -28,7 +28,7 @@ class Election(object):
     def races(self):
         response = self.session.get(self.url,
                                     params={'election': self.elec_code})
-        page = lxml.html.fromstring(response.text)
+        page = lxml.html.fromstring(response.content.decode('utf-8'))
         option_els = page.xpath(
             "//select[@name='race']/option")
 
@@ -48,7 +48,7 @@ class Election(object):
         return races
     
 class Race(object):
-    RESULTS_URL = 'https://chicagoelections.com/en/data-export.asp'
+    RESULTS_URL = 'https://chicagoelections.gov/en/election-results-specifics.asp'
     
     def __init__(self, elec_code, name=None, number=None, session=None):
         self.elec_code = elec_code
@@ -72,33 +72,36 @@ class Race(object):
         results_d = {}
         
         response = self.session.get(self.RESULTS_URL,
-                                    params = {'election': self.elec_code,
-                                              'race': self.number})
-        page = lxml.html.fromstring(response.text)
+                                    data={'election': self.elec_code,
+                                          'race': self.number})
+        page = lxml.html.fromstring(response.content.decode('utf-8'))
 
         tables = page.xpath('//table')
-
-        total = tables.pop(0)
-        header_row = total.xpath('./tr')[0]
-        keys = ['precinct']
-        for cell in header_row.xpath('./td//text()'):
-            keys.append(cell.strip().replace("''", "'"))
+        tables.pop(0)  # Discard total
 
         for table in tables:
-            rows = table.xpath('./tr')
-            ward = rows.pop(0).xpath('./td//text()')[0]
+            title, header = table.xpath('./thead/tr')
+            ward, = title.xpath('./th/b/text()')
             ward_num = int(ward.split()[-1])
-            rows.pop(0) # ignore repeated header row
-            for row in rows:
+            keys = [field.strip() for field in header.xpath('./td/b/text()')]
+
+            for row in table.xpath('./tr'):
                 votes = {}
+
                 for key, cell in zip(keys, row.xpath('./td//text()')):
-                    if cell == 'Total': # ignore ward subtotals
+                    if cell == 'Total':  # Ignore ward subtotals
                         break
-                    if '%' in key or key == 'Votes': # ignore derived vars
+
+                    if key == 'Precinct':
+                        precinct = int(cell)
                         continue
+
+                    elif key == '%':  # Ignore derived vars
+                        continue
+
                     votes[key] = int(cell.strip().replace(',', ''))
+
                 else:
-                    precinct = votes.pop('precinct')
                     results_d[(ward_num, precinct)] = votes
 
         return results_d
@@ -132,7 +135,7 @@ class Race(object):
 def elections(session=None):
     '''List all available elections'''
 
-    election_url = 'https://chicagoelections.com/en/election-results.html'
+    election_url = 'https://chicagoelections.gov/en/election-results.html'
 
     if session is None:
         session = requests.Session()
@@ -140,7 +143,7 @@ def elections(session=None):
         session = session
     
     response = session.get(election_url)
-    page = lxml.html.fromstring(response.text)
+    page = lxml.html.fromstring(response.content.decode('utf-8'))
 
     election_links = page.xpath("//a[starts-with(@href, 'election-results.asp?election=')]")
 
